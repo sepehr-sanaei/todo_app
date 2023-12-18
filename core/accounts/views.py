067463f9282af54cee719a104cde620283aaf54f
sepehr-sanaei <sepehrsanaeiazad@gmail.com>
2023-12-18 14:52:55 +0330

    Done creating accounts app

---
+++ core/accounts/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView, View
+from django.contrib.auth import logout
+# Create your views here.
+
+class LoginView(LoginView):
+    redirect_authenticated_user = True
+    
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+    
+    def form_invalid(self, form):
+        messages.error(self.request, 'Invalid username or password')
+        return self.render_to_response(self.get_context_data(form=form))
+    
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
+    
+    
+class LogOutView(View):
+    def get(self, request):
+        logout(request)
+        return redirect('/accounts/login/')
+    
+     
