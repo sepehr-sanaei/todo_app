e0029335e0d345c218eaa3f67737505f5abf2db8
sepehr-sanaei <sepehrsanaeiazad@gmail.com>
2023-12-20 14:37:48 +0330

    almost done

--- core/todo/urls.py
+++ core/todo/urls.py
@@ -5,5 +5,8 @@ app_name = 'todo'
 
 urlpatterns = [
     path('', views.CreateTasksView.as_view(), name='tasks'),
-    path('tasks/', views.TasksListView.as_view(), name='Tasks-List')
+    path('tasks/', views.TasksListView.as_view(), name='Tasks-List'),
+    path('edit/<int:pk>/', views.EditTaskView.as_view(), name='edit-task'),
+    path('delete/<int:pk>/', views.DeleteTaskView.as_view(), name='delete-task'),
+    path('complete/<int:pk>/', views.TaskCompleteView.as_view(), name='complete-task'),
 ]
--- core/todo/views.py
+++ core/todo/views.py
@@ -1,7 +1,8 @@
+from django.shortcuts import redirect
 from django.forms.models import BaseModelForm
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView, View
 from .models import Tasks
 from .forms import TasksForm
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -25,3 +26,21 @@ class CreateTasksView(LoginRequiredMixin, CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
     
+class EditTaskView(LoginRequiredMixin,UpdateView):
+    model = Tasks
+    form_class = TasksForm
+    success_url = "/tasks/"
+
+class DeleteTaskView(LoginRequiredMixin, DeleteView):
+    model = Tasks
+    success_url = "/tasks/"
+    
+class TaskCompleteView(LoginRequiredMixin, View):
+    model = Tasks
+    success_url = "/tasks/"
+    
+    def get(self, request, *args, **kwargs):
+        object = Tasks.objects.get(id=kwargs.get("pk"))
+        object.task_complete = True
+        object.save()
+        return redirect(self.success_url)    
